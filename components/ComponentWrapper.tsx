17559be6286bc2bbfa213420e8e5ab764c90f9ad
HerilS <herilsaha06@gmail.com>
2022-09-03 15:32:22 -0600

    feat(docs/grid): added grid docs

--- components/ComponentWrapper.tsx
+++ components/ComponentWrapper.tsx
@@ -4,29 +4,47 @@ import theme from "../components/theme";
 
 interface ComponentWrapperProps {
   children?: React.ReactNode;
+  noFlex?: boolean;
 }
 
-const ComponentWrapper = ({ children }: ComponentWrapperProps) => (
+const ComponentWrapper = ({
+  children,
+  noFlex = false,
+}: ComponentWrapperProps) => (
   <DecaUIProvider
     root={false}
     theme={{ ...baseTheme.theme, fonts: theme.fonts }}
   >
-    <Box
-      css={{
-        display: "flex",
-        justifyContent: "flex-start",
-        alignItems: "flex-end",
-        gap: "$3",
-        p: "$3",
-        br: "$md",
-        borderStyle: "solid",
-        borderWidth: "$normal",
-        borderColor: "$gray100",
-        flexWrap: "wrap",
-      }}
-    >
-      {children}
-    </Box>
+    {noFlex ? (
+      <Box
+        css={{
+          p: "$3",
+          br: "$md",
+          borderStyle: "solid",
+          borderWidth: "$normal",
+          borderColor: "$gray100",
+        }}
+      >
+        {children}
+      </Box>
+    ) : (
+      <Box
+        css={{
+          display: "flex",
+          justifyContent: "flex-start",
+          alignItems: "flex-end",
+          gap: "$3",
+          p: "$3",
+          br: "$md",
+          borderStyle: "solid",
+          borderWidth: "$normal",
+          borderColor: "$gray100",
+          flexWrap: "wrap",
+        }}
+      >
+        {children}
+      </Box>
+    )}
   </DecaUIProvider>
 );
 
