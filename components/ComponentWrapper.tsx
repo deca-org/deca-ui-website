597892aacde27a2567261b39c089a5c8d1f39d86
Heril Saha <herilsaha06@gmail.com>
2022-09-04 02:15:44 -0600

    feat(docs/text): added text docs

--- components/ComponentWrapper.tsx
+++ components/ComponentWrapper.tsx
@@ -5,11 +5,13 @@ import theme from "../components/theme";
 interface ComponentWrapperProps {
   children?: React.ReactNode;
   noFlex?: boolean;
+  column?: boolean;
 }
 
 const ComponentWrapper = ({
   children,
   noFlex = false,
+  column = false,
 }: ComponentWrapperProps) => (
   <DecaUIProvider
     root={false}
@@ -32,7 +34,7 @@ const ComponentWrapper = ({
         css={{
           display: "flex",
           justifyContent: "flex-start",
-          alignItems: "flex-end",
+          alignItems: column ? "flex-start" : "flex-end",
           gap: "$3",
           p: "$3",
           br: "$md",
@@ -41,6 +43,7 @@ const ComponentWrapper = ({
           borderColor: "$gray100",
           flexWrap: "wrap",
           overflow: "auto",
+          flexDirection: column ? "column" : "row",
         }}
       >
         {children}
